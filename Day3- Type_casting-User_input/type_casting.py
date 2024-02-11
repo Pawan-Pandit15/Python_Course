ef3d2d488bebf0381f73d51067265e74edd47383
Pawan <Pawanpandit332@gmail.com>
2024-02-11 12:51:16 +0530

    comment,print,escape sequence,variable,datatype

---
+++ Day3- Type_casting-User_input/type_casting.py
@@ -0,0 +1,24 @@
+# Type Casting
+
+a="1"   # here 1 is String
+b="2"   # here 2 is String
+print(a+b)   # here answer is 12 because both are string
+
+c=int(a)  # type casting convert string to integer
+d=int(b)
+print(c+d)
+
+print(float(a)+float(b))   # convert string to float
+
+x=2
+y=10
+print(str(x)+str(y))       # convert integer to string
+
+
+
+# Implicit TypeCasting
+
+c = 1.9
+d = 8
+
+print(c + d)
