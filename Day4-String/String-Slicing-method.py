d77f57a9d8e9839ce28b332e35770917d2898d71
Pawan <Pawanpandit332@gmail.com>
2024-02-11 19:53:41 +0530

    String

---
+++ Day4-String/String-Slicing-method.py
@@ -0,0 +1,60 @@
+# String
+
+# name='Pawan'
+# surname="Pandit"
+# poem='''Twinkle, twinkle, little star        # this all 3 is a String
+# How I wonder what you are
+# Up above the world so high
+# Like a diamond in the sky…'''
+#
+# print(name)
+# print(surname)
+# print(poem)
+
+
+# String Slicing ( indexing )
+
+# name="Pawan"
+# print(name[2])
+# print(name[:2])
+# print(name[1:3])
+# print(name[-2])
+# print(name[:-2])
+# print(name[-4:-2])
+
+
+# String Slicing with skip value
+
+# name="PawanPandit"
+# print(name[0:11:2])
+# print(name[::3])
+
+
+# String Itration using for Loop
+
+# name="Pawan"
+
+# for a in name:
+#     print(a)
+
+
+
+# String Function ( Method )
+
+name="!!!Pawan Pandit!!!"
+print(name)
+print(len(name))
+print(name.count("a"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.find("i"))
+print(name.replace("Pandit","Laptop"))
+
+
+
+
+
+
+
+
