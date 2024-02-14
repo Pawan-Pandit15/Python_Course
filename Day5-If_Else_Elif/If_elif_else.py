6029ff63b4ee7ece1ec9d1207db6f4a2f09debbf
Pawan <Pawanpandit332@gmail.com>
2024-02-14 13:26:40 +0530

    match_case

---
+++ Day5-If_Else_Elif/If_elif_else.py
@@ -0,0 +1,18 @@
+# a=int(input("Enter Total marks :"))
+# if a>=90:
+#     print("You are Pass From 'A+'")
+# elif a>=70 and a<=89:
+#     print("You are Pass From 'A'")
+# elif a>=60 and a<=69:
+#     print("You are Pass From 'B'")
+# elif a>=35 and a<=59:
+#     print("You are Pass From 'C'")
+# else:
+#     print("You are Fail")
+
+
+
+
+
+
+
---
+++ Day6-match_case/match_case.py
@@ -0,0 +1,53 @@
+# # # Match Case
+# # # Switch
+# # numbers = int(input("Enter a Number 1-6:\n"))
+# # match numbers: #BREAK IS NOT NEEDED in case of Match and CASE
+# #     case 1:
+# #         print("You have entered 1")
+# #     case 2:
+# #         print("You have entered 2")
+# #     case 3:
+# #         print("You have entered 3")
+# #     case 4:
+# #         print("You have entered 4")
+# #     case 5:
+# #         print("You have entered 5")
+# #     case 6:
+# #         print("You have entered 6")
+# #     case _:
+# #         print("No idea")
+#
+#
+#
+#
+# x = int(input("Enter the value of x: "))
+# # x is the variable to match
+# match x:
+#     # if x is 0
+#     case 0:
+#         print("x is zero")
+#     # case with if-condition
+#     case 4:
+#         print("case is 4")
+#
+#     case _ if x!=90:
+#         print(x, "is not 90")
+#     case _ if x!=80:
+#         print(x, "is not 80")
+#     case _:
+#         print(x)
+
+
+
+
+browser = str(input("Enter the browser name:\n"))
+browser = browser.lower()
+match browser:
+    case "chrome":
+        print("Chrome code executed!")
+    case "firefox":
+        print("Firefox  code executed!")
+    case "google":
+        print("Google browser executed!")
+    case _:
+        print("No browser Found!")
