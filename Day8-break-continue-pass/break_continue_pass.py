a9181fa04db018853510d62873b05db48a2f0849
Pawan <Pawanpandit332@gmail.com>
2024-02-17 16:27:16 +0530

    break-continue-pass

---
+++ Day8-break-continue-pass/break_continue_pass.py
@@ -0,0 +1,43 @@
+
+# break ( Break the code )
+
+# for i in range(6):
+#     print(i)
+#     if i==3:
+#         break             # here break means out the loop
+# print("break statement excuted")
+
+
+
+# continue skip the code
+
+
+# for i in range(6):
+#     if i==3:
+#         print("Skip the value")
+#         continue    # here continue skip the value in loop
+#     print(i)
+
+
+
+# pass ->?
+# skip -> the code
+
+# for i in range(6):
+#     if i == 3:
+#         print(" skip the value ")
+#         pass        # here pass skip the value in loop
+#     else:
+#         print(i)
+
+
+
+
+
+
+
+
+
+
+
+
