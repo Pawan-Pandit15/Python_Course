c3e49d0216b80d4406469f6a363a63f270d7b5a6
Pawan <Pawanpandit332@gmail.com>
2024-02-18 16:16:16 +0530

    Tuple and methods

---
+++ Day11-Tuple/tuple-methods.py
@@ -0,0 +1,56 @@
+# Tuple - Collection of items
+# Tuple is Unmutable
+# Tuple not changeble if you want to change tuple then convert tuple to list
+
+# t=(1,3,4,"pawan",True)
+# print(type(t))
+# print(t)
+#
+# print(t[3])    # indexing
+# print(t[-2])   # Negative indexing
+# print(len(t))  # tuple length
+# l=list(t)      # Convert tuple to list
+# print(l)
+# print(type(l))
+#
+# t1=tuple(l)   # Convert list to Tuple
+# print(t1)
+# print(type(t1))
+
+
+# API_URLS = ("www.google.com", "www.facebook.com", "www.instagram.com")
+# print(API_URLS[0])
+# print(API_URLS[1])      # Tuple Indexing
+# print(API_URLS[2])
+
+
+# tuple3 = tuple(["Pramod", "Lucky"])
+# print(tuple3)
+# print(tuple3[1])
+# print(type(tuple3))
+
+
+
+
+
+# Nested Tuples
+
+#
+# hero1 = ("Batman", "Bruce Wayne")
+# hero2 = ("Wonder Woman", "Diana Prince")
+# awesome_team = (hero1, hero2)
+# print(awesome_team,type(awesome_team))
+# print(awesome_team[0][1])
+# print(awesome_team[1][1])
+
+
+
+
+# # Search in Tuple
+
+# cities = ("London", "Paris", "Los Angeles", "Tokyo")
+# print("Paris" in cities)
+# print("Moscow" in cities)
+
+
+
