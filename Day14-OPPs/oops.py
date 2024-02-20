5f3880bc3a1cc650e6741bf939fb37729b5d4fa4
Pawan <Pawanpandit332@gmail.com>
2024-02-20 16:47:51 +0530

    OPPs

---
+++ Day14-OPPs/oops.py
@@ -0,0 +1,98 @@
+# Class & Objects in Python
+# Class -  Attributes and Behaviour
+
+# Person -> Object Pawan, Basant, Ram
+
+
+# RailwayForm   ---> Class [blueprint]
+# harry --> Pawan ki info wala form --> Object [entity]
+# tom --> tom ki info wala form --> Object [entity]
+# shubham -- shubham ki info wala form --> Object [entity]
+# shubham.changeName("Shubhi")
+
+
+
+# class myclass:
+#     def myfunction(self):
+#         print("Hello myfunction")
+#
+# m1=myclass()
+# m1.myfunction()
+
+
+# class Cal:
+#     def sum(self, a, b):
+#         return a + b
+#
+#     def sub(self, a, b):
+#         return a - b
+#
+#     def mul(self, a, b):
+#         return a * b
+#
+#     def div(self, a, b):
+#         return a / b
+#
+#
+# obj= Cal()
+# result =obj.sum(3, 4)
+# print(result)
+
+
+# class Mul_param:
+#     name = None  # Class Variable
+#
+#     def print_information(self, first_name, last_name, age):
+#         a = 10  # Local Variable
+#         print("Your name is ", first_name, last_name, age)
+#         print(self.name)
+#
+#
+# obj_ref = Mul_param()
+# obj_ref.print_information("Amit", "Sharma", 68)
+
+
+# class Car:
+#     color = None
+#     model = None
+#
+#     def car_details(self):
+#         print("Your car details is ", self.color, self.model)
+#
+#
+# car_color = input("Enter you car Color\n")
+# car_model = input("Enter you car Model\n")
+#
+# # car_obj_ref = Car()
+# car_obj_ref = Car()
+#
+# car_obj_ref.color = car_color
+# car_obj_ref.model = car_model
+#
+# car_obj_ref.car_details()
+
+
+# class Person:
+#   name = "Pawan"
+#   occupation = "Software Developer"
+#   networth = 10
+#   def info(self):
+#     print(f"{self.name} is a {self.occupation}")
+#
+#
+# a = Person()
+# b = Person()
+# c = Person()
+#
+# a.name = "Shubham"
+# a.occupation = "Accountant"
+#
+# b.name = "Nitika"
+# b.occupation = "HR"
+#
+# # print(a.name, a.occupation)
+# a.info()
+# b.info()
+# c.info()
+
+
