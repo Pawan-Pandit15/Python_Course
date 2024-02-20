197db04b368842b6a232e4fbd2a175719b5f2dae
Pawan <Pawanpandit332@gmail.com>
2024-02-20 16:55:17 +0530

    Match_Case

--- Day6-match_case/match_case.py
+++ Day6-match_case/match_case.py
@@ -59,19 +59,19 @@
 #
 
 #
-#
+
 # browser = str(input("Enter the browser name:\n"))
 # browser = browser.lower()
 # match browser:
 #     case "chrome":
 #         print("Chrome code executed!")
 #     case "firefox":
-#         print("Firefox  code executed!")
+#         print("Firefox code executed!")
 #     case "google":
 #         print("Google browser executed!")
-#     case _ if "browser==fb":
+#     case browser if browser == "fb":
 #         print("you are enter facebook")
-#     case _ if "browser==insta":
+#     case browser if browser == "insta":
 #         print("you are enter instagram")
 #     case _:
-#         print("No browser Found!")   # some error in this code
+#         print("No browser Found!")
