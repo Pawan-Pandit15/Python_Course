62d57b498a307cab186b05e6f8f9af37a87d9dfc
Pawan <Pawanpandit332@gmail.com>
2024-03-06 17:16:42 +0530

    Polumorphism

---
+++ Day15-Polymorphism/Overloading.py
@@ -0,0 +1,28 @@
+# Poly - many - Object-oriented programming (OOP)
+# Morphism - Form
+
+# Method overloading
+# Method overriding
+
+#---------Method Overloading ------------
+
+''' Method Overloading is not supported in python because python is a
+      interpreted language python work line by line.'''
+
+class Mo:
+    def myfunction(self):
+        print("function with no argument")
+
+    def myfunction(self,a):
+        print("function with 1 argument")
+
+    def myfunction(self,a,b):
+        print("function with 2 argument")
+
+obj=Mo()
+obj.myfunction(10,20)
+'''here when i call function with no argument and 1 argument show error because 
+    pyhton is a interpreter language call line by line that's why 
+     overloading not support in python'''
+
+
---
+++ Day15-Polymorphism/Overriding.py
@@ -0,0 +1,54 @@
+# Method Overriding
+# Method Overriding is the runtime polymorphism
+# Method Overriding means having same name method, but into different class, and argument are also same
+# In method Overriding inheritence is complusary
+
+# child always overide the parent functions
+# super means call my parent function
+
+# Method overriding is same like Multilevel Inheritance
+
+# class M1:                      # Parent class no use super method
+#     def myfunction(self,a):
+#         print("Class m1 function is called")
+#
+# class M2(M1):
+#     def myfunction(self,a):
+#         print("Class m2 function is called")
+#         super().myfunction(10)
+#
+# class M3(M2):
+#     def myfunction(self,a):
+#         print("Class m3 function is called")
+#         super().myfunction(10)    # here super function use complusary in overriding
+#                                   # when use super function call there upside function
+# obj=M3()
+# obj.myfunction(10)
+
+
+
+#-----------------------------------------------------------------
+
+# class Animal:
+#
+#     def __init__(self):
+#         pass
+#
+#     def sound(self):
+#         print("Animal Sound")
+#
+#
+# class Dog(Animal):
+#
+#     def __init__(self):
+#         pass
+#
+#     def sound(self):
+#         super().sound()
+#         print("Dog Sound")
+#
+#
+#
+#
+# dog = Dog()
+# dog.sound()
