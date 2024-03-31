4be41b63439f682b34cd9608a648b6a3806a47ff
Pawan <Pawanpandit332@gmail.com>
2024-03-31 16:51:27 +0530

    API Testing_1

--- pytest/test_file.py
+++ pytest/test_file.py
@@ -1,8 +1,19 @@
 import pytest
 
 
-def test_sub1():
+def test_sub():
     assert 2 - 2 == 0
+
+def test_add():
+    assert 2 + 2 == 4
+
+def test_mul():
+    assert 2 * 3 == 5
+
+def test_div():
+    assert 2 / 2 != 0
+
+
 
 
 # @pytest.mark.smoke
@@ -12,7 +23,6 @@ def test_sub1():
 #
 # @pytest.mark.reg
 # def test_sub3():
-#     assert 2 - 2 == 0
 #
 #
 # @pytest.mark.reg
