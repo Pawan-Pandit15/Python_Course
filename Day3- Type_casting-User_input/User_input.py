f11370eab364e64d09dc51e9043b6b8181676851
Pawan <Pawanpandit332@gmail.com>
2024-07-08 13:55:04 +0530

    API_Testing

--- Day3- Type_casting-User_input/User_input.py
+++ Day3- Type_casting-User_input/User_input.py
@@ -3,7 +3,7 @@ print("My name is", a)
 
 x = input("Enter first number: ")  # when not define any date type in user input then always String type
 y = input("Enter second number: ")
-print(x  + y)
+print(x + y)
 
 print(int(x) + int(y))     # here type casting convert string to integer
 
